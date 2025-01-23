867ca36b08549d2aa3d09841eef89a4d7b80f3d3
siranjeevan <smartjeevith2k@gamil.com>
2025-01-23 16:33:34 +0530

    Initial Commit

---
+++ SWIFT LEARNING/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWIFT LEARNING
+//
+//  Created by Jeevith  on 23/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWIFT LEARNING/SWIFT_LEARNINGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFT_LEARNINGApp.swift
+//  SWIFT LEARNING
+//
+//  Created by Jeevith  on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SWIFT_LEARNINGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
